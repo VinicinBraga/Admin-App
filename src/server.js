3bf9873a2f80eaf752ac09acdf4e6b8420d461fd
VinicinBraga <vinicius@bragamatta.com.br>
2022-05-11 17:37:14 -0300

    Initial Setup

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import AdminJS from "adminjs";
+import AdminJSExpress from "@adminjs/express";
+
+const express = require("express");
+const app = express();
+
+const adminJS = new AdminJS({
+  database: [],
+  rootPath: "/admin",
+  resources: [],
+});
+
+const router = AdminJSExpress.buildRouter(adminJS);
+
+app.use(adminJS.options.rootPath, router);
+app.listen(3000, () => {
+  console.log("adminJs is under http://localhost:3000/admin");
+});
